1b6708c680b95531678c6ed52c4fea9dcca9bb2a
Roman Orlov <asindor@gmail.com>
2016-03-16 22:45:49 +0300

    Удаление всякого барахла

--- src/main/java/lgk/nsbc/ru/App.java
+++ src/main/java/lgk/nsbc/ru/App.java
@@ -3,28 +3,15 @@ package lgk.nsbc.ru;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
-import com.vaadin.data.util.converter.Converter;
-import com.vaadin.server.Resource;
-import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Grid;
-import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.renderers.*;
-import lgk.nsbc.ru.model.ConsultationModel;
-import lgk.nsbc.ru.presenter.ConsultationPresenter;
-import lgk.nsbc.ru.view.ConsultationView;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Locale;
 
 /**
  *
@@ -34,36 +21,7 @@ import java.util.Locale;
 public class App extends UI {
 
     @Override
-    protected void init(VaadinRequest vaadinRequest) {/*
-		String lgkSessionId = getPage().getUriFragment();
-		getPage().setUriFragment(null, false);
-
-		PatientsModelImpl model = new PatientsModelImpl(new I18nManager());
-		PatientsList view = new PatientsList(model);
-		setContent(view);
-
-		PatientsManager patientsManager = new PatientsManager();
-		SessionManager sessionManager = new SessionManager();
-		PatientsPresenter patientsPresenter = new PatientsPresenter(model, patientsManager, sessionManager, lgkSessionId);
-		patientsPresenter.start();*/
-		ConsultationModel model = new ConsultationModel();
-		ConsultationPresenter presenter = new ConsultationPresenter(model);
-		presenter.start();
-		ConsultationView view = new ConsultationView(model);
-		setContent(view);
-		/*Grid grid= new Grid("");
-		grid.addColumn("text",String.class);
-		TextFieldRenderer<String> textFieldRenderer = new TextFieldRenderer<>();
-		grid.getColumn("text").setRenderer(textFieldRenderer);
-		grid.addRow("1");
-		grid.addRow("2");
-		grid.addRow("3");
-		grid.addRow("4");
-		grid.addRow("5");
-		grid.addRow("6");
-		grid.addRow("7");
-		grid.addRow("8");
-		setContent(grid);*/
+    protected void init(VaadinRequest vaadinRequest) {
 	}
 
 	@WebServlet(urlPatterns = "/*", name = "AppServlet", asyncSupported = true)
--- src/main/java/lgk/nsbc/ru/client/textfieldrenderer/TextFieldRendererServerRpc.java
+++
@@ -1,8 +0,0 @@
-package lgk.nsbc.ru.client.textfieldrenderer;
-
-import com.vaadin.shared.communication.ServerRpc;
-
-public interface TextFieldRendererServerRpc extends ServerRpc {
-
-    public void onChange(String rowKey, String columnId, String newValue);
-}
